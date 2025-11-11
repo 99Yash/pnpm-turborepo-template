7eb0bf839690cfb1dbdb00accabbb2f7b18a8eca
yash <yashgouravkar@gmail.com>
2025-11-11 18:25:24 +0530

    initing template

--- apps/web/src/lib/auth/server.ts
+++ apps/web/src/lib/auth/server.ts
@@ -1,7 +1,7 @@
+import { db } from '@ciaran/db';
+import * as schema from '@ciaran/db/schemas';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { db } from '~/db';
-import * as schema from '~/db/schemas';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
